dcb500ed19e862283d6e36486e8d270f6f58c5e5
Antonio Garcia Giron <antoniogarciagiron@gmail.com>
2021-01-23 20:33:10 +0100

    Limpieza terminada

--- src/funciones_toni.py
+++ src/funciones_toni.py
@@ -17,3 +17,27 @@ def monthattack (x, pattern):
         return res[0]
     else:
         return "NaN"
+
+def daytime (x, pattern):
+    import re
+    res = re.findall(pattern, x)
+    res="".join(res)
+    if res in ["06","07","08","09","10","11", "Morning"]:
+        return "Morning"
+    elif res in ["12","13","14","15","16","17", "Afternoon"]:
+        return "Afternoon"
+    elif res in ["18","19","20","21","22","23", "Evening"]:
+        return "Evening"
+    elif res in ["24","01","02","03","04","05", "Night"]:
+        return "Night"
+    else:
+        return "NaN"
+
+def fatal (x, pattern):
+    import re
+    res = re.findall(pattern, x)
+    res="".join(res)
+    if len(res)>=1:
+        return res
+    else:
+        return "NaN"
